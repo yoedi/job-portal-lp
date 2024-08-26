daf90e76f7d68288f960d6d050c7498e8285aa8e
Yoedi Arianto <yoediarianto@gmail.com>
2024-08-26 07:47:30 +0700

    Component client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Hero from "@/components/organism/Hero";
+import Clients from "@/components/organism/Clients";
 
 export default function Home() {
   return (
-    <div className="px-32">
+    <div className="px-32 mt-10">
       <Hero />
+      <Clients />
     </div>
   );
 }
