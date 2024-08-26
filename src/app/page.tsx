97181b335493654094143522e2119c5a53a57094
Yoedi Arianto <yoediarianto@gmail.com>
2024-08-26 08:06:51 +0700

    Create category component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Hero from "@/components/organism/Hero";
 import Clients from "@/components/organism/Clients";
+import Category from "@/components/organism/Category";
 
 export default function Home() {
   return (
     <div className="px-32 mt-10">
       <Hero />
       <Clients />
+      <Category />
     </div>
   );
 }
