bab005b2bf8a95df823f70aeab49e11bdda618a6
Yoedi Arianto <yoediarianto@gmail.com>
2024-08-28 21:34:55 +0700

    create browse company page

--- src/app/find-companies/page.tsx
+++ src/app/find-companies/page.tsx
@@ -1,9 +1,48 @@
+"use client";
+
+import { formFilterCompanySchema } from "@/lib/form-schema";
 import React, { FC } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { filterFormType } from "@/types";
+import { CATEGORIES_OPTIONS } from "@/constans";
+import ExploreDataContainer from "@/containers/ExplorerDataContainer";
 
 interface FindCompaniesProps {}
 
+const FILTER_FORMS: filterFormType[] = [
+  {
+    name: "industry",
+    label: "Industry",
+    items: CATEGORIES_OPTIONS,
+  },
+];
+
 const FindCompanies: FC<FindCompaniesProps> = ({}) => {
-  return <div></div>;
+  const formFilter = useForm<z.infer<typeof formFilterCompanySchema>>({
+    resolver: zodResolver(formFilterCompanySchema),
+    defaultValues: {
+      industry: [],
+    },
+  });
+
+  const onSubmitFormFilter = async (
+    val: z.infer<typeof formFilterCompanySchema>
+  ) => console.log(val);
+
+  return (
+    <ExploreDataContainer
+      formFilter={formFilter}
+      onSubmitFilter={onSubmitFormFilter}
+      filterForm={FILTER_FORMS}
+      loading={false}
+      title="dream companies"
+      subTitle="Find the dream companies you dream work for"
+      type="company"
+      data={[]}
+    />
+  );
 };
 
 export default FindCompanies;
