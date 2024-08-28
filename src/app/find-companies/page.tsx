8ba0dacdabbccd1f8ffd1394baa7e2c3e22cb977
Yoedi Arianto <yoediarianto@gmail.com>
2024-08-28 21:23:07 +0700

    adjust navbar link

---
+++ src/app/find-companies/page.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface FindCompaniesProps {}
+
+const FindCompanies: FC<FindCompaniesProps> = ({}) => {
+  return <div></div>;
+};
+
+export default FindCompanies;
