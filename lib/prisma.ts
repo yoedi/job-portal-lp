0a610272dcc85dc399e94d41e57d1c2aa2c789e6
Yoedi Arianto <yoediarianto@gmail.com>
2024-09-12 13:55:50 +0700

    Install and config prisma, supabase, and nextauth

---
+++ lib/prisma.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+let prisma: PrismaClient;
+
+declare const globalThis: {
+  prisma: PrismaClient;
+};
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!globalThis.prisma) {
+    globalThis.prisma = new PrismaClient();
+  }
+
+  prisma = globalThis.prisma;
+}
