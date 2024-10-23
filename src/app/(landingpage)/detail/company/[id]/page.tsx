a9792447171c8c2e2c20727a2d413481f9e33cbb
Yoedi Arianto <yoediarianto@gmail.com>
2024-10-23 21:07:25 +0700

    Integrate detail company

--- src/app/(landingpage)/detail/company/[id]/page.tsx
+++ src/app/(landingpage)/detail/company/[id]/page.tsx
@@ -17,10 +17,51 @@ import {
   HiOutlineLocationMarker,
   HiOutlineOfficeBuilding,
 } from "react-icons/hi";
+import prisma from "../../../../../../lib/prisma";
+import { supabasePublicUrl } from "@/lib/supabase";
+import { dateFormat } from "@/lib/utils";
+import { CompanyTeam } from "@prisma/client";
 
-interface DetailCompanyPageProps {}
+type params = {
+  id: string;
+};
 
-const DetailCompanyPage: FC<DetailCompanyPageProps> = ({}) => {
+interface DetailCompanyPageProps {
+  params: params;
+}
+
+async function getDetailCompany(id: string) {
+  const data = await prisma.company.findFirst({
+    where: { id },
+    include: {
+      CompanyOverview: true,
+      CompanySocialMedia: true,
+      CompanyTeam: true,
+      _count: {
+        select: {
+          Job: true,
+        },
+      },
+    },
+  });
+
+  let imageUrl;
+
+  if (data?.CompanyOverview[0].image) {
+    imageUrl = await supabasePublicUrl(
+      data.CompanyOverview[0].image,
+      "company"
+    );
+  } else {
+    imageUrl = "/images/company2.png";
+  }
+
+  return { ...data, imageUrl };
+}
+
+const DetailCompanyPage: FC<DetailCompanyPageProps> = async ({ params }) => {
+  const data = await getDetailCompany(params.id);
+
   return (
     <>
       <div className="bg-slate-100 px-32 pt-16 pb-14">
@@ -38,28 +79,30 @@ const DetailCompanyPage: FC<DetailCompanyPageProps> = ({}) => {
           /{" "}
           <Link
             className="hover:underline hover:text-black"
-            href="/detail/company/1"
+            href={`/detail/company/${data.id}`}
           >
-            Twitter
+            {data.CompanyOverview[0].name}
           </Link>
         </div>
 
         <div>
           <div className="mt-10 inline-flex gap-6 items-start">
             <Image
-              src="/images/company2.png"
-              alt="/images/company2.png"
+              src={data.imageUrl}
+              alt={data.imageUrl}
               width={150}
               height={150}
             />
             <div>
               <div className="inline-flex gap-4 items-center">
-                <span className="text-4xl font-semibold">Twitter</span>
-                <Badge>10 Jobs</Badge>
+                <span className="text-4xl font-semibold">
+                  {data.CompanyOverview[0].name}
+                </span>
+                <Badge>{data._count?.Job} Jobs</Badge>
               </div>
               <div className="mt-2">
                 <Link href="/" className="font-semibold text-primary">
-                  https://twitter.com
+                  {data.CompanyOverview[0].website}
                 </Link>
               </div>
 
@@ -67,22 +110,25 @@ const DetailCompanyPage: FC<DetailCompanyPageProps> = ({}) => {
                 <CompanyInformation
                   Icon={AiOutlineFire}
                   label="Founded"
-                  value="March, 06 2007"
+                  value={dateFormat(
+                    data.CompanyOverview[0].dateFounded,
+                    "MMM, DD YYYY"
+                  )}
                 />
                 <CompanyInformation
                   Icon={BsPeople}
-                  label="Founded"
-                  value="151-200"
+                  label="Employees"
+                  value={data.CompanyOverview[0].employee}
                 />
                 <CompanyInformation
                   Icon={HiOutlineLocationMarker}
                   label="Location"
-                  value="Indonesia"
+                  value={data.CompanyOverview[0].location}
                 />
                 <CompanyInformation
                   Icon={HiOutlineOfficeBuilding}
                   label="Industry"
-                  value="Advertising"
+                  value={data.CompanyOverview[0].industry}
                 />
               </div>
             </div>
@@ -93,29 +139,33 @@ const DetailCompanyPage: FC<DetailCompanyPageProps> = ({}) => {
         <div className="w-3/4">
           <div className="mb-16">
             <div className="text-3xl font-semibold mb-3">Company Profile</div>
-            <div className="text-muted-foreground">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptas consequatur voluptates corporis repellat nobis optio
-                illum. Magnam illum commodi nobis! Suscipit magnam repudiandae
-                velit doloremque beatae nihil vero dignissimos sed.
-              </p>
-            </div>
+            <div
+              className="text-muted-foreground"
+              dangerouslySetInnerHTML={{
+                __html: data.CompanyOverview[0].description,
+              }}
+            ></div>
           </div>
           <div>
             <div className="text-3xl font-semibold mb-4">Contact</div>
             <div className="flex flex-wrap items-center gap-5 w-[400px] ">
               <div className="p-2 border border-primary text-primary w-max inline-flex items-center gap-3 font-semibold">
                 <FacebookIcon />
-                <span className="text-sm">https://facebook.com/twitter</span>
+                <span className="text-sm">
+                  {data.CompanySocialMedia[0].facebook}
+                </span>
               </div>
               <div className="p-2 border border-primary text-primary w-max inline-flex items-center gap-3 font-semibold">
                 <TwitterIcon />
-                <span className="text-sm">https://twitter.com/twitter</span>
+                <span className="text-sm">
+                  {data.CompanySocialMedia[0].twitter}
+                </span>
               </div>
               <div className="p-2 border border-primary text-primary w-max inline-flex items-center gap-3 font-semibold">
                 <LinkedinIcon />
-                <span className="text-sm">https://linkedin.com/twitter</span>
+                <span className="text-sm">
+                  {data.CompanySocialMedia[0].linkedin}
+                </span>
               </div>
             </div>
           </div>
@@ -125,9 +175,12 @@ const DetailCompanyPage: FC<DetailCompanyPageProps> = ({}) => {
           <div className="text-gray-500 text-sm">
             Learn about the technology and tools that Pattern uses.
           </div>
-          <div className="mt-5 inline-flex gap-4">
-            <Badge>Javascript</Badge>
-            <Badge>HTML</Badge>
+          <div className="mt-5 flex flex-row items-center text-wrap gap-4">
+            {data.CompanyOverview[0].techStack.map(
+              (item: string, i: number) => (
+                <Badge key={item + i}>{item}</Badge>
+              )
+            )}
           </div>
         </div>
       </div>
@@ -136,12 +189,12 @@ const DetailCompanyPage: FC<DetailCompanyPageProps> = ({}) => {
         <div className="my-16">
           <div className="text-3xl font-semibold mb-4">Teams</div>
           <div className="grid grid-cols-5 gap-5 mt-5">
-            {[0, 1, 2, 3, 4].map((i: number) => (
-              <div key={i} className="border border-border px-3 py-5">
+            {data.CompanyTeam.map((data: CompanyTeam) => (
+              <div key={data.id} className="border border-border px-3 py-5">
                 <div className="w-16 h-16 rounded-full mx-auto bg-gray-100" />
                 <div className="text-center my-4">
-                  <div className="font-semibold text-sm">Yoedi</div>
-                  <div className="text-gray-500 text-xs">CEO & Co-Founder</div>
+                  <div className="font-semibold text-sm">{data.name}</div>
+                  <div className="text-gray-500 text-xs">{data.position}</div>
                 </div>
 
                 <div className="mx-auto w-max">
--- src/components/organism/CompanyCard/index.tsx
+++ src/components/organism/CompanyCard/index.tsx
@@ -12,21 +12,17 @@ const CompanyCard: FC<CompanyCardProps> = ({
   image,
   name,
   totalJobs,
+  id,
 }) => {
   const router = useRouter();
 
   return (
     <div
-      onClick={() => router.push("/detail/company/1")}
+      onClick={() => router.push("/detail/company/" + id)}
       className="border border-border p-6 cursor-pointer"
     >
       <div className="flex flex-row justify-between items-start">
-        <Image
-          src="/images/company2.png"
-          alt="/images/company2.png"
-          width={66}
-          height={66}
-        />
+        <Image src={image} alt={image} width={66} height={66} />
         <Badge>{totalJobs} Jobs</Badge>
       </div>
       <div className="my-4">
